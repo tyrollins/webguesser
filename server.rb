d6510309325e6f5953b09bee76ec5b7283d2861a
stoked <tylerrollins@outlook.com>
2014-10-04 18:38:57 -0700

    display message high/low/correct

--- server.rb
+++ server.rb
@@ -2,6 +2,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(100)
+
+def check_guess(guess, number)
+  if number > guess
+    'Too high!'
+  elsif number < guess
+    'Too low!'
+  else
+    "Got it!"
+  end
+end
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  message = check_guess(guess, number)
+  erb :index, :locals => {:number => number, :message => message}
 end
